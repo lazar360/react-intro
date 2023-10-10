84fee60c7e0b9f8b8bbc62a6293ec16df5145625
NicolasGautier <nicolas.gautier35135@gmail.com>
2023-10-10 22:04:10 +0200

    maj script

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { Greetings } from "./Greetings";
 import { AgeCounter } from "./AgeCounter";
+import "./global.css";
 
 export function App() {
   return (
     <>
-      <h1>Hello</h1>
-      <Greetings 
-      firstName={"Rachelle"}
-      isSunny = {false}
-      />
-      <AgeCounter/>
+    <Greetings></Greetings>
+     <div style={{
+      backgroundColor:"red",
+      height:100,
+      width:100,
+      border:"3px solid blue"
+     }}></div> 
+
+
     </>
   );
 }
