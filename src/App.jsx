e0372be99518f90f4df1567532f99f36b240004c
Nicolas Gautier <91179295+lazar360@users.noreply.github.com>
2023-10-13 16:11:20 +0200

    Update App.jsx cf ordi cgi

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
+import { MenuList } from "./components/MenuList/MenuList.jsx";
 import { DisplayDifficulty } from "./components/DisplayDifficulty/DisplayDifficulty.jsx";
+import s from "./style.module.css";
 
 export function App() {
+  const [currentDifficulty, setCurrentDifficulty] = useState("");
+
+  function updateDifficulty(difficulty) {
+    setCurrentDifficulty(difficulty);
+  }
+
   return (
-  <>
-  <DisplayDifficulty />
-  </>
-  )
-  
+    <div>
+      <h1 className={s.title}>Select your React difficulty</h1>
+      <div className={s.workspace}>
+        <MenuList
+          onItemClick={updateDifficulty}
+          difficulty={currentDifficulty}
+        />
+        <DisplayDifficulty difficulty={currentDifficulty} />
+      </div>
+    </div>
+  );
 }
+
